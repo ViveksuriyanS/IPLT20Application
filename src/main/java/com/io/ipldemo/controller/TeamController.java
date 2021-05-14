ccebe2466b68e5894a39e0b5d909401ad7fcbd5b
ViveksuriyanS <viveksuriyancs93@gmail.com>
2021-05-14 19:44:34 +0530

    React with Team Page

--- src/main/java/com/io/ipldemo/controller/TeamController.java
+++ src/main/java/com/io/ipldemo/controller/TeamController.java
@@ -3,6 +3,7 @@ package com.io.ipldemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.io.ipldemo.service.MatchService;
 import com.io.ipldemo.service.TeamService;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
 	@Autowired
